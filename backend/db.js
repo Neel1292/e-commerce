3ccc806fb25df60b875e591a2b66011e81f340e1
Neel Prajapti <prajapatineel122002@gmail.com>
2024-05-27 18:16:17 +0530

    Vercel Added

--- backend/db.js
+++ backend/db.js
@@ -3,14 +3,8 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
-const pool = new Pool(
-  {
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    host : process.env.HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DATABASE
-  }
-);
+const pool = new Pool({
+  connectionString: process.env.POSTGRES_URL,
+})
 
 module.exports = pool;
