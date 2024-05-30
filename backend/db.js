a80f7e7d28fd57477763d2497b68d68b529139c7
Neel Prajapti <prajapatineel122002@gmail.com>
2024-05-30 13:46:16 +0530

    updated

--- backend/db.js
+++ backend/db.js
@@ -4,11 +4,7 @@ const dotenv = require("dotenv");
 dotenv.config();
  
 const pool = new Pool({
-    user: "sa@postgresqlashish",
-    password: "Hypeteq@2023",
-    host : "postgresqlashish.postgres.database.azure.com",
-    port: 5432,
-    database: "NeelNodeTraining"
-});
-  
+  connectionString: process.env.POSTGRES_URL,
+})
+ 
 module.exports = pool;
